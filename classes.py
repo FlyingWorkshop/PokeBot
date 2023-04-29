a95d7b63e4b3269b5fac8bacc49b0abcc53bd3fb
FlyingWorkshop <loganbham@yahoo.com>
2023-04-28 17:17:28 -0700

    more data

--- classes.py
+++ classes.py
@@ -1,5 +1,14 @@
 class Pokemon:
-    pass
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.gender = ""
+        self.item = ""
+        self.ability = ""
+        self.evs = {"hp": 0, "atk": 0, "def": 0, "spa": 0, "spd": 0, "spe": 0}
+        self.nature = ""
+        self.moves = ""
+
 
 
 class Battle:
--- references.md
+++ references.md
@@ -4,3 +4,4 @@
 * [Poke-env](https://poke-env.readthedocs.io/en/stable/cross_evaluate_random_players.html#getting-something-to-run): 
   * for connected to the showdown server
   * not intended to train agents (but we might still be able to)
+* [randbats](https://pkmn.github.io/randbats/) API for getting list of random Pokemon
