7031dd4a6136faaf8dca9a3e3f4e72338f3389dc
Adam Zhao <adamzhao@stanford.edu>
2023-05-08 15:45:47 -0700

    Initial thoughts/things

--- alphagogoat/delphox.py
+++ alphagogoat/delphox.py
@@ -2,6 +2,7 @@ from torch import nn
 import torch
 
 NUM_MOVES = 4
+NUM_INPUT = 81
 
 class Delphox(nn.Module):
     def __init__(self):
@@ -11,6 +12,12 @@ class Delphox(nn.Module):
         self.stack = nn.Sequential(
             nn.Softmax(n, )
         )
+
+        #broken up network here:
+        #-------------------------
+        # pass matrix through a linear layer (flaten maybe), to get a distributioin over all possible actions.
+        # then set all invalid options to -inf
+        # then softmax, select action
 
 
     def forward(self, x):
