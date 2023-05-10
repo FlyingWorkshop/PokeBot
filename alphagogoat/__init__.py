1af5dd02dd334723eb0b7b9b01d6c89d1a3814ff
FlyingWorkshop <loganbham@yahoo.com>
2023-05-10 16:24:24 -0700

    cron testing

--- alphagogoat/__init__.py
+++ alphagogoat/__init__.py
@@ -1,1 +1,9 @@
-from . import classes, utils
+URL = "https://replay.pokemonshowdown.com/"
+
+class Scraper:
+    def __init__(self):
+        pass
+
+
+    def open(self):
+        pass
---
+++ connect.py
@@ -0,0 +1,37 @@
+import asyncio
+import json
+
+from poke_env.player import RandomPlayer
+from poke_env import ShowdownServerConfiguration, PlayerConfiguration
+
+
+async def main():
+    with open("account.json") as f:
+        data = json.load(f)
+
+
+    # We create a random player
+    player = RandomPlayer(
+        player_configuration=PlayerConfiguration(data['username'], data['password']),
+        server_configuration=ShowdownServerConfiguration,
+    )
+
+    # Sending challenges to 'your_username'
+    # await player.send_challenges('flyingworkshop', n_challenges=1)
+
+    # Accepting one challenge from any user
+    await player.accept_challenges(None, 1)
+
+    # Accepting three challenges from 'your_username'
+    # await player.accept_challenges('your_username', 3)
+
+    # Playing 5 games on the ladder
+    # await player.ladder(5)
+
+    # Print the rating of the player and its opponent after each battle
+    # for battle in player.battles.values():
+    #     print(battle.rating, battle.opponent_rating)
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
