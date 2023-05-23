bb5dea07a8bdbdc1bc8bc6b91682940c00edda13
FlyingWorkshop <loganbham@yahoo.com>
2023-05-23 14:40:00 -0700

    stuff

--- alphagogoat/delphox.py
+++ alphagogoat/delphox.py
@@ -1,18 +1,17 @@
 import torch
 import torch.nn as nn
 
+from poke_env.environment.battle import Battle
 
-# Define the neural network class
 class Delphox(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
+        # TODO: make Delphox a RNN or LSTM; perhaps use meta-learning
         super().__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.Softmax()
+        self.rnn = nn.LSTM(1, 2, 3)
 
 
-    def forward(self, environment, team1, team2):
+    def forward(self, battle: Battle):
+        team1, team2 = get_teams(battle)
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
