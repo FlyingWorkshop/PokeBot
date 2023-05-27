bb0fdb6b5cdde0316a5d112e512d24a83acc10f9
Adam Zhao <adamzhao@stanford.edu>
2023-05-27 10:51:02 -0700

    started on gogoat

--- alphagogoat/gogoat.py
+++ alphagogoat/gogoat.py
@@ -2,6 +2,80 @@ import numpy as np
 from gym.spaces import Space, Box
 from poke_env.player import Gen8EnvSinglePlayer
 from poke_env.environment.abstract_battle import AbstractBattle
+import math
+import random
+import torch
+
+
+
+class Node:
+    def __init__(self, state, parent=None):
+        self.state = state
+        self.parent = parent
+        self.children = []
+        self._number_of_visits = 0
+        self._total_reward = 0
+
+    def number_of_visits(self):
+        return self._number_of_visits
+
+    def total_reward(self):
+        return self._total_reward
+
+    def is_fully_expanded(self):
+        return len(self.untried_actions()) == 0
+
+    def best_child(self, c_param=1.4):
+        choices_weights = [
+            c.total_reward() / (c.number_of_visits()) + c_param * math.sqrt((2 * math.log(self.number_of_visits()) / (c.number_of_visits())))
+             for c in self.children
+        ]
+        return self.children[choices_weights.index(max(choices_weights))]
+
+    def rollout_policy(self, possible_moves):        
+        return possible_moves[random.randint(0, len(possible_moves) - 1)]
+
+    def expand(self):
+        action = self.untried_actions().pop()
+        next_state = self.state.move(action)
+        child_node = Node(next_state, parent=self)
+        self.children.append(child_node)
+        return child_node
+
+    def rollout_policy(self, possible_moves, net):
+        # Convert your state to a tensor
+        state_tensor = torch.tensor(self.state).float()
+        # Pass state through the network
+        action_probabilities = net(state_tensor)
+        # Select action based on the output of your network
+        action = torch.argmax(action_probabilities).item()
+        return action
+    
+    def backpropagate(self, reward):
+        self._number_of_visits += 1.
+        self._total_reward += reward
+        if self.parent:
+            self.parent.backpropagate(reward)
+
+class MCTS:
+    def __init__(self, root):
+        self.root = root
+
+    def search(self, iterations):
+        for _ in range(iterations):
+            node = self._tree_policy()
+            reward = node.rollout()
+            node.backpropagate(reward)
+        return self.root.best_child(c_param=0.)
+
+    def _tree_policy(self):
+        current_node = self.root
+        while not current_node.state.is_terminal():
+            if not current_node.is_fully_expanded():
+                return current_node.expand()
+            else:
+                current_node = current_node.best_child()
+        return current_node
 
 
 
